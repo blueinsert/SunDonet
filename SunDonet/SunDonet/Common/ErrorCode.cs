4f75cf883d56a9b30df1a4f5e1c040f876c4b0fc
zhongzhengxiang <zhongzhengxiang@blackjack-inc.com>
2023-09-07 10:47:16 +0800

    new

---
+++ SunDonet/SunDonet/Common/ErrorCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunDonet
+{
+    public class ErrorCode
+    {
+        public const int OK = 0;
+        public const int LoginPasswordError = 10;
+        public const int LoginAccountNotExist = 11;
+    }
+}
